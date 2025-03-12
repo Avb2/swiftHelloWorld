417d3e8d17b8260ddf91a0ac8aa9a609ad8faa6e
Avb2 <bringuel.alexander@gmail.com>
2025-03-11 21:12:53 -0400

    Added showOutput func

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,7 +10,15 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var txtName: UITextField!
     @IBOutlet weak var lblOutput: UIView!
-
+    
+    
+    @IBAction func showOutput(_ sender: UIButton) {
+        let _name = txtName.text
+        let output = "Hello" + _name!
+        lblOutput.text = output
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       

7d9e05a70630346a2b62f65774c51123c7cc5421
Avb2 <bringuel.alexander@gmail.com>
2025-03-11 22:17:37 -0400

    Finished code

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -9,19 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var txtName: UITextField!
-    @IBOutlet weak var lblOutput: UIView!
+    
+    @IBOutlet weak var lblOutput: UILabel!
+    
     
     
     @IBAction func showOutput(_ sender: UIButton) {
         let _name = txtName.text
-        let output = "Hello" + _name!
+        let output = "Hello " + (_name ?? "Enter your name")
         lblOutput.text = output
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap: UIGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
       
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
 
 }

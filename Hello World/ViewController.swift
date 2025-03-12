a6f4707efc4344880f19f95a8f59fa02ac7fd3be
Avb2 <bringuel.alexander@gmail.com>
2025-03-11 21:01:06 -0400

    ..

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var lblOutput: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
-
 
 }
 
